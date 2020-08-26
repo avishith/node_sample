1441ccfcacc96ca0151aa1ca76d055ed22a44992
Avishith PM <pmshithu143@gmail.com>
2020-08-26 23:53:01 +0530

    learned to add html file to node

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var http=require('http')
+var fs=require('fs')
 http.createServer(function(req,res){
-    res.write("	AVISHITH PM ")
-    	res.end()
+    fs.readFile('example.html',function (err,data){
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.write(data)
+    res.end()
+})  
+    
 }).listen(8080)
---
+++ xample.js
@@ -0,0 +1,2 @@
+var dt= new Date()
+console.log(dt)
